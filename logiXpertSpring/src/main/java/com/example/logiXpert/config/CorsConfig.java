d9ddbffc4c7c26d4131100dddb797ed4ac55cd06
Todor Mitev <td_mitev@abv.bg>
2024-11-26 17:19:28 +0200

    [Todor]: Added CORS config

---
+++ logiXpertSpring/src/main/java/com/example/logiXpert/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.logiXpert.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Разреши всички URL пътища
+                        .allowedOrigins("http://localhost:4200") // Angular приложението
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешени методи
+                        .allowedHeaders("*") // Разрешени хедъри
+                        .allowCredentials(true); // Позволи изпращане на креденциали (ако е нужно)
+            }
+        };
+    }
+}
