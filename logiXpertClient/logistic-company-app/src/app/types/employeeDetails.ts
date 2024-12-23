ba07aee21f15dab66d627ea999a22b63e14600e7
Todor Mitev <td_mitev@abv.bg>
2024-12-23 17:16:08 +0200

    [Todor]: Add employee and office interfaces

---
+++ logiXpertClient/logistic-company-app/src/app/types/employeeDetails.ts
@@ -0,0 +1,11 @@
+export interface EmployeeDetails {
+    id: number;
+    name: string;
+    phone: string;
+    email: string;
+    salary: number;
+    officeName: string;
+    companyName: string;
+    vehicleId: number;
+    role: string;
+  }
