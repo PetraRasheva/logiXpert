c7a639ef827b02feea6aeae95672f608d4eb46fa
VanessaGK03 <vanesa.kostova.2003@gmail.com>
2024-12-23 18:52:38 +0200

    [Vanessa]: Add my-shipments component

---
+++ logiXpertClient/logistic-company-app/src/app/pages/profile/my-shipments/my-shipments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-shipments',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-shipments.component.html',
+  styleUrl: './my-shipments.component.css'
+})
+export class MyShipmentsComponent {
+
+}
